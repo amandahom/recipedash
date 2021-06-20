df11b730186112f84829b8d5467ff58d2fc82cd9
Mandy Hom <amandakhom@gmail.com>
2021-06-20 16:03:52 -0700

    Fixing navbar padding

--- assets/components/Navbar.tsx
+++ assets/components/Navbar.tsx
@@ -49,7 +49,7 @@ function Navbar() {
               {() => (
                 <>
                   <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div className="flex items-center justify-between h-18">
+                    <div className="flex items-center justify-between h-20">
                       <div className="flex items-center">
                         <div className="flex-shrink-0">
                           <a href="/">
@@ -83,7 +83,7 @@ function Navbar() {
               {({ open }) => (
                 <>
                   <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div className="flex items-center justify-between h-16">
+                    <div className="flex items-center justify-between h-20">
                       <div className="flex items-center">
                         <div className="flex-shrink-0">
                           <a href="/">
