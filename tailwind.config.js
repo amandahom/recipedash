fbf1fef0880fd672b1cc129aa5a4574b25d21aea
Mandy Hom <amandakhom@gmail.com>
2021-06-20 16:12:01 -0700

    Fixing tailwind purge settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
     './pages/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './components/*.{js,ts,jsx,tsx}',
+    './assets/**/**/*.{js,ts,jsx,tsx}',
+    './assets/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
